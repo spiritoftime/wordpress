0cf88149776c2ea563c99678c13caf6a786e9d7e
spiritoftime <chooliangyi6@gmail.com>
2023-06-20 22:36:03 +0800

    installed shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import { useAuth0 } from "@auth0/auth0-react";
+import { AlertDialogDemo } from "./components/AlertDialog";
 
 function App() {
   const { isAuthenticated, isLoading } = useAuth0();
 
-  return <></>;
+  return <AlertDialogDemo></AlertDialogDemo>;
 }
 
 export default App;
