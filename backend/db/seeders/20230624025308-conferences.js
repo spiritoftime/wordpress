d240cb552dd2aa7a2bfb993bc8730a9c484bf916
hWeitian <huangweitian1@hotmail.com>
2023-07-01 10:29:11 +0800

    Update seed file

--- backend/db/seeders/20230624025308-conferences.js
+++ backend/db/seeders/20230624025308-conferences.js
@@ -7,25 +7,25 @@ module.exports = {
         start_date: "2021-06-24",
         end_date: "2021-06-25",
         name: "APARCS 2022",
-        country:"USA",
-        venue:"New York",
+        country: "USA",
+        venue: "New York",
+        wordpressApi: "asdasd123123123",
       },
       {
-
         start_date: "2021-05-24",
         end_date: "2021-05-25",
         name: "APARCS 2021",
-        country:"USA",
-        venue:"New York",
-
+        country: "USA",
+        venue: "New York",
+        wordpressApi: "asdasd78678678",
       },
       {
-
         start_date: "2021-04-24",
         end_date: "2021-04-25",
         name: "APARCS 2020",
-        country:"USA",
-        venue:"Washington",
+        country: "USA",
+        venue: "Washington",
+        wordpressApi: "asdasd9089018234",
       },
     ]);
   },
