957463fa411b11fb2a035709eaaf0b657e55f332
spiritoftime <chooliangyi6@gmail.com>
2023-07-01 15:32:12 +0800

    create normal combo box component that is not part of forms

--- frontend/src/components/Conference.jsx
+++ frontend/src/components/Conference.jsx
@@ -1,5 +1,30 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import useGetAccessToken from "../custom_hooks/useGetAccessToken";
+import { getConferences } from "../services/conferences";
+import { useState } from "react";
+import { NormalComboBox } from "./NormalComboBox";
+
 const Conference = () => {
-  return <div className="flex flex-col"></div>;
+  const getAccessToken = useGetAccessToken();
+
+  const { data: conferences, isLoading: isConferenceFetching } = useQuery({
+    queryKey: ["conferences"],
+    queryFn: async () => {
+      const accessToken = await getAccessToken();
+      return getConferences(accessToken);
+    },
+    refetchOnWindowFocus: false, // it is not necessary to keep refetching
+  });
+  return (
+    <div className="flex flex-col">
+      <NormalComboBox
+        options={conferences}
+        validateProperty={"name"}
+        displayProperty={"name"}
+        fieldName={"conference"}
+      />
+    </div>
+  );
 };
 
 export default Conference;
---
+++ frontend/src/components/NormalComboBox.jsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "../lib/utils";
+import { Button } from "./ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "./ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+
+// normal comboBox that is not part of a react hook form.
+export function NormalComboBox({
+  options,
+  validateProperty,
+  displayProperty,
+  fieldName,
+}) {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? options.find((option) => option.value === value)?.label
+            : `Select ${fieldName}...`}
+          <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search framework..." />
+          <CommandEmpty>No framework found.</CommandEmpty>
+          <CommandGroup>
+            {options &&
+              options.map((option) => (
+                <CommandItem
+                  key={option[validateProperty]}
+                  value={option[validateProperty]}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      option[validateProperty] === value[validateProperty]
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                  {option[displayProperty]}
+                </CommandItem>
+              ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
