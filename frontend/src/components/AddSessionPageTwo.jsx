6bfa5f8f61ff898df5b70f3e9121d372738de5f1
hWeitian <huangweitian1@hotmail.com>
2023-07-24 11:25:37 +0800

    Remove unwanted code

--- frontend/src/components/AddSessionPageTwo.jsx
+++ frontend/src/components/AddSessionPageTwo.jsx
@@ -13,44 +13,6 @@ const AddSessionPageTwo = ({ control, topicsForAddingSession }) => {
   // console.log("selectedtopics", selectedTopics);
   const { conferenceId } = useParams();
   console.log("conference id in addSessionPageTwo: ", conferenceId);
-  // const getAccessToken = useGetAccessToken();
-
-  // const {
-  //   data: topicsForAddingSession,
-  //   isLoading: isTopicsForAddingSessionLoading,
-  //   isFetching: isTopicsForAddingSessionFetching,
-  // } = useQuery({
-  //   queryKey: ["topicsForAddingSession"],
-  //   queryFn: async () => {
-  //     const accessToken = await getAccessToken();
-  //     const data = await getTopicsForAddingToSession(accessToken, conferenceId);
-  //     const res = data.map((topic) => {
-  //       const obj = {};
-  //       obj.title = topic.title;
-  //       obj.speaker = topic.Speakers.map((speaker) => speaker.fullName);
-  //       obj.country = topic.Speakers.map((speaker) => speaker.country);
-  //       obj.speakersId = topic.Speakers.map((speaker) => speaker.id);
-  //       obj.numSessions = topic.Speakers.map(
-  //         (speaker) => speaker.Sessions.length
-  //       );
-  //       obj.topicId = topic.id;
-  //       obj.speakerPostId = topic.Speakers.map(
-  //         (speaker) => speaker.Conferences[0].ConferenceSpeaker.speakerPostId
-  //       );
-  //       obj.speakerLink = topic.Speakers.map(
-  //         (speaker) => speaker.Conferences[0].ConferenceSpeaker.speakerLink
-  //       );
-  //       return obj;
-  //     });
-  //     return res;
-  //   },
-  //   refetchOnWindowFocus: false, // it is not necessary to keep refetching
-  // });
-
-  // useEffect(() => {
-  //   console.log("useEffect ran in AddSessionPageTwo");
-  //   refetchTopics();
-  // }, []);
 
   console.log(topicsForAddingSession, "topicsForAddingSession");
   // const topics = [
