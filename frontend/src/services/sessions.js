36eea5b2a752d9cfc031c115d651c997a5e51830
hWeitian <huangweitian1@hotmail.com>
2023-07-24 16:39:05 +0800

    Add function to get total symposia and masterclass count for each conference

--- frontend/src/services/sessions.js
+++ frontend/src/services/sessions.js
@@ -3,18 +3,32 @@ import { makeRequest } from "./makeRequest";
 export function getSessions(accessToken, conferenceId) {
   return makeRequest(`/sessions/conference/${conferenceId}`, accessToken);
 }
+
+export function getSymposiaCount(accessToken, conferenceId) {
+  return makeRequest(`/sessions/symposia-count/${conferenceId}`, accessToken);
+}
+
+export function getMasterclassCount(accessToken, conferenceId) {
+  return makeRequest(
+    `/sessions/masterclass-count/${conferenceId}`,
+    accessToken
+  );
+}
+
 export function addSession(accessToken, conferenceId, data) {
   return makeRequest(`/sessions/conference/${conferenceId}`, accessToken, {
     method: "POST",
     data: data,
   });
 }
+
 export function getSession(accessToken, conferenceId, sessionId) {
   return makeRequest(
     `/sessions/conference/${sessionId}/${conferenceId}`,
     accessToken
   );
 }
+
 export function updateSession(accessToken, conferenceId, sessionId, data) {
   return makeRequest(
     `/sessions/conference/${sessionId}/${conferenceId}`,
