cede2d3899e17c2f2af69367bb746c268b5f14f6
hWeitian <huangweitian1@hotmail.com>
2023-07-27 15:49:43 +0800

    Update appContext file name

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AppProvider } from "./context/AppContext.jsx";
+import { AppProvider } from "./context/appContext.jsx";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
